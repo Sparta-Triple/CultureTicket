2b8c28d7005a6184f6a411f0a6ed6eb8354e5f97
kwj0605 <rladnwlsok@naver.com>
2025-01-03 02:29:20 +0900

    feat 'Base' 생성 #5

---
+++ com.culture-ticket.client.reservation-payment/src/main/java/com/culture_ticket/client/reservation_payment/common/Base.java
@@ -0,0 +1,58 @@
+package com.culture_ticket.client.reservation_payment.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Base {
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    private String createdBy;
+
+    private String updatedBy;
+
+    private String deletedBy;
+
+    private Boolean isDeleted = false;
+
+    protected void createdBy(String email) {
+        this.createdBy = email;
+        this.updatedBy = email;
+    }
+
+    protected void updatedBy(String email){
+        this.updatedBy = email;
+    }
+
+    protected void softDeletedBy(String email) {
+        this.isDeleted = true;
+        this.deletedBy = email;
+        this.deletedAt = LocalDateTime.now();
+    }
+
+    protected void restoreBy(String email){
+        this.isDeleted = false;
+        this.deletedBy = null;
+        this.deletedAt = null;
+    }
+}
