bdf5ed309a4c7b03096df5ed056e09ad524d062a
yurim <y1551463@gmail.com>
2025-01-03 14:28:39 +0900

    refactor ResponseEntity 제외 (pr 반영) #6

--- com.culture-ticket.client.reservation-payment/src/main/java/com/culture_ticket/client/reservation_payment/presentation/controller/PaymentController.java
+++ com.culture-ticket.client.reservation-payment/src/main/java/com/culture_ticket/client/reservation_payment/presentation/controller/PaymentController.java
@@ -1,8 +1,8 @@
 package com.culture_ticket.client.reservation_payment.presentation.controller;
 
-import com.culture_ticket.client.reservation_payment.application.dto.requestDto.SeatSelectionRequest;
-import com.culture_ticket.client.reservation_payment.application.dto.responseDto.CreatePaymentResponse;
-import com.culture_ticket.client.reservation_payment.application.dto.responseDto.PaymentResponse;
+import com.culture_ticket.client.reservation_payment.application.dto.requestDto.SeatSelectionRequestDto;
+import com.culture_ticket.client.reservation_payment.application.dto.responseDto.CreatePaymentResponseDto;
+import com.culture_ticket.client.reservation_payment.application.dto.responseDto.PaymentResponseDto;
 import com.culture_ticket.client.reservation_payment.application.service.PaymentService;
 import com.culture_ticket.client.reservation_payment.common.ResponseDataDto;
 import com.culture_ticket.client.reservation_payment.common.ResponseStatus;
@@ -10,7 +10,6 @@ import jakarta.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,22 +26,22 @@ public class PaymentController {
     private final PaymentService paymentService;
 
     @PostMapping("")
-    public ResponseEntity<ResponseDataDto<CreatePaymentResponse>> createPayment(
-        @Valid @RequestBody SeatSelectionRequest request) { //TODO 헤더 데이터 추가
-        return ResponseEntity.ok(new ResponseDataDto<>(ResponseStatus.PAYMENT_SUCCESS,
-            paymentService.createPayment(1L, request)));
+    public ResponseDataDto<CreatePaymentResponseDto> createPayment(
+        @Valid @RequestBody SeatSelectionRequestDto request) { //TODO 헤더 데이터 추가
+        return new ResponseDataDto<>(ResponseStatus.PAYMENT_SUCCESS,
+            paymentService.createPayment(1L, request));
     }
 
     @GetMapping("")
-    public ResponseEntity<ResponseDataDto<List<PaymentResponse>>> getPaymentList() {
-        return ResponseEntity.ok(new ResponseDataDto<>(ResponseStatus.GET_PAYMENT_SUCCESS,
-            paymentService.getPaymentList(1L)));
+    public ResponseDataDto<List<PaymentResponseDto>> getPaymentList() {
+        return new ResponseDataDto<>(ResponseStatus.GET_PAYMENT_SUCCESS,
+            paymentService.getPaymentList(1L));
     }
 
     @GetMapping("/{paymentId}")
-    public ResponseEntity<ResponseDataDto<PaymentResponse>> getPayment(
+    public ResponseDataDto<PaymentResponseDto> getPayment(
         @PathVariable UUID paymentId) {
-        return ResponseEntity.ok(new ResponseDataDto<>(ResponseStatus.GET_PAYMENT_SUCCESS,
-            paymentService.getPayment(1L, paymentId)));
+        return new ResponseDataDto<>(ResponseStatus.GET_PAYMENT_SUCCESS,
+            paymentService.getPayment(1L, paymentId));
     }
 }
