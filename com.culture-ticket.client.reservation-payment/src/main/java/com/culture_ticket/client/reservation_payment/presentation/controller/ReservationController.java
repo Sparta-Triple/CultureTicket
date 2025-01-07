25067c82af200921552b2fc8cbcd7ae800848c8a
Woojin Kim <rladnwlsok@naver.com>
2025-01-07 12:36:01 +0900

    refactor api 복수로 수정 #5

--- com.culture-ticket.client.reservation-payment/src/main/java/com/culture_ticket/client/reservation_payment/presentation/controller/ReservationController.java
+++ com.culture-ticket.client.reservation-payment/src/main/java/com/culture_ticket/client/reservation_payment/presentation/controller/ReservationController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/reservation")
+@RequestMapping("api/v1/reservations")
 @RequiredArgsConstructor
 public class ReservationController {
 
