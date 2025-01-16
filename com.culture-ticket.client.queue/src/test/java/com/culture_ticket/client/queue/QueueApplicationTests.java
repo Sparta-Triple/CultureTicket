e4fd3caffbfa78ecd57896a7170b1b32fc8614fd
kwj0605 <rladnwlsok@naver.com>
2025-01-16 10:37:58 +0900

    setup 대기열 프로젝트 생성 #42

---
+++ com.culture-ticket.client.queue/src/test/java/com/culture_ticket/client/queue/QueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.culture_ticket.client.queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
