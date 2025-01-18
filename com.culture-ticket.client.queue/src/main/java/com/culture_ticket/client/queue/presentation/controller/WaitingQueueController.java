f813217e5d85d2f9cce3962d779f10aa2959d5c6
Woojin Kim <rladnwlsok@naver.com>
2025-01-19 01:44:46 +0900

    setup 대기열 프로젝트 생성 #42

---
+++ com.culture-ticket.client.queue/src/main/java/com/culture_ticket/client/queue/presentation/controller/WaitingQueueController.java
@@ -0,0 +1,13 @@
+package com.culture_ticket.client.queue.presentation.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/queues")
+public class WaitingQueueController {
+
+
+}
