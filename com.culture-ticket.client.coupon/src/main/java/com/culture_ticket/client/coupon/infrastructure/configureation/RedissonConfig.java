b847eb6b0fc4abcca4f1afb936b38427d7c36cce
Woojin Kim <rladnwlsok@naver.com>
2025-01-24 02:45:02 +0900

    fix 배포하기 위해 redisson 수정 #39

--- com.culture-ticket.client.coupon/src/main/java/com/culture_ticket/client/coupon/infrastructure/configureation/RedissonConfig.java
+++ com.culture-ticket.client.coupon/src/main/java/com/culture_ticket/client/coupon/infrastructure/configureation/RedissonConfig.java
@@ -19,7 +19,7 @@ public class RedissonConfig {
         RedissonClient redisson = null;
         Config config = new Config();
         config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort)
-            .setUsername("default").setPassword("systempass");
+            .setUsername("default");
         redisson = Redisson.create(config);
         return redisson;
     }
