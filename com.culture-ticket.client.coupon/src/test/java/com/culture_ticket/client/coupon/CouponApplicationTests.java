8075e0b0a0a4d2e4151c670e6166af96f109952a
Woojin Kim <rladnwlsok@naver.com>
2025-01-24 03:00:50 +0900

    fix 배포하기 위해 testcode 수정 #39

--- com.culture-ticket.client.coupon/src/test/java/com/culture_ticket/client/coupon/CouponApplicationTests.java
+++ com.culture-ticket.client.coupon/src/test/java/com/culture_ticket/client/coupon/CouponApplicationTests.java
@@ -3,11 +3,11 @@ package com.culture_ticket.client.coupon;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CouponApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class CouponApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
